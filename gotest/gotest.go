bbfd3690dda7f2fdc00240fd0df466a1b1542992
jiaoiqang <jiaoqiang1@users.noreply.github.com>
2016-05-10 14:31:57 +0800

    Update gotest.go

--- gotest/gotest.go
+++ gotest/gotest.go
@@ -5,9 +5,9 @@ import (
 )
 
 func Division(a, b float64) (float64, error) {
-    if b == 0 {
+    if b==0 {
         return 0, errors.New("除数不能为0")
     }
 
-    return a / b, nil
+    return a/b, nil
 }
