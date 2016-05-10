97dcc740f733d2a8d33f40ac458b3e43eec19fb5
jiaoiqang <jiaoqiang1@users.noreply.github.com>
2016-05-10 14:05:49 +0800

    Update max_test.go

--- max_test.go
+++ max_test.go
@@ -1,15 +1,20 @@
-package main
+package gotest
 
-import "testing"
+import (
+    "testing"
+)
 
-func TestMain(t *testing.T) {
-   /* 定义局部变量 */
-   var a int = 100
-   var b int = 200
-   var ret int
-
-   /* 调用函数并返回最大值 */
-   ret = Max(a, b)
-
-   t.Printf( "最大值是 : %d\n", ret )
+func Test_Division_1(t *testing.T) {
+    if i, e := Division(6, 2); i != 3 || e != nil { //try a unit test on function
+        t.Error("除法函数测试没通过") // 如果不是如预期的那么就报错
+    } else {
+        t.Log("第一个测试通过了") //记录一些你期望记录的信息
+    }
 }
+
+func Test_Division_2(t *testing.T) {
+    t.Error("就是不通过")
+}
+我们在项目目录下面执行go test,就会显示如下信息：
+
+--- FAIL: Test_Division_2 (0
