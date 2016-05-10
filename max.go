fd904da19e89bf82a3d3949ac39eea44fed1f1d4
jiaoiqang <jiaoqiang1@users.noreply.github.com>
2016-05-10 12:55:21 +0800

    Create max.go

---
+++ max.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func max(num1, num2 int) int {
+   /* 定义局部变量 */
+   var result int
+
+   if (num1 > num2) {
+      result = num1
+   } else {
+      result = num2
+   }
+   return result 
+}
